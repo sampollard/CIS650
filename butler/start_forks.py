b98978b763f2994d8c589af3d5b5cf383eaaf2b9
robert <rjs_00@msn.com>
2017-10-27 06:05:45 +0000

    Start scripts added

--- butler/start_forks.py
+++ butler/start_forks.py
@@ -16,5 +16,19 @@ except:
     print("Pass in a int")
     sys.exit(1)
 
+processwait =[]
 for i in range(int(sys.argv[1])):
-    subprocess.call(["python", "forks.py", str(myList[i])])
+    try:
+        p = subprocess.Popen(['python', 'fork.py', str(myList[i])])
+        processwait.append(p)
+        print("started " + str(myList[i]))
+    except EnvironmentError as e:  
+        sys.exit('failed to start %r, reason: %s' % (executable, e))
+        
+#Wait for each process to end, for call control c and kill all child processes
+while len(processwait)>1:
+    try: # wait for the child process to finish
+        for each in processwait:
+            each.wait()
+    except KeyboardInterrupt: 
+        sys.exit("interrupted")
